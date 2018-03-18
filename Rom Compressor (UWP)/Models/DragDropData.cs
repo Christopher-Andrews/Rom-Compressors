f696eeef43b98aa09d6acc7da8a988bceafdbbf3
Christopher Andrews <thesolemnspirit@googlemail.com>
2018-03-18 16:48:42 +0000

    Add project files.

---
+++ Rom Compressor (UWP)/Models/DragDropData.cs
@@ -0,0 +1,11 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+
+namespace Rom_Compressor__UWP_.Models
+{
+    public class DragDropData
+    {
+        public DataPackageOperation AcceptedOperation { get; set; }
+
+        public DataPackageView DataView { get; set; }
+    }
+}
